cb6c73259fdd9a20ac6dbd3d9635671f43c238f4
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-04-07 13:11:30 +0200

    Add sample.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Spring Cloud Square Retrofit Web Sample
+Example of using Retrofit with Spring Cloud LoadBalancer integration and Eureka
+
+Assumes eureka (from [spring-cloud-samples](https://github.com/spring-cloud-samples/eureka)) is running on http://localhost:8761
+
+## building
+
+`mvnw package`
+
+## hello server
+
+run `java -jar server/target/retrofit-web-server-0.0.1-SNAPSHOT.jar`
+
+verify it is functioning at [http://localhost:7111](http://localhost:7111)
+
+You should see `Hello World: HelloServer:myhostname:7111`
+
+## hello client
+
+run `java -jar client/target/retrofit-web-client-0.0.1-SNAPSHOT.jar`
+
+verify it is functioning at [http://localhost:7211](http://localhost:7211)
+
+You should see `Hello World: HelloServer:myhostname:7111`
+
+### hello client error
+
+You may see an error while the eureka/loadbalancer caches warm up similar to the following:
+
+    Load balancer does not contain an instance for the service HelloServer
+
+It should go away shortly.
+
+## See round robin load-balancing in action
+
+run `java -jar server/target/retrofit-web-server-0.0.1-SNAPSHOT.jar --server.port=7112`
+
+Go back to [http://localhost:7211](http://localhost:7211) and you should see both ports `7111` and `7112` in the output after a minute or two as you keep refreshing.
---
+++ client/src/main/java/demo/HelloClientApplication.java
@@ -0,0 +1,50 @@
+package demo;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.square.retrofit.EnableRetrofitClients;
+import org.springframework.cloud.square.retrofit.core.RetrofitClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Olga Maciaszek-Sharma
+ */
+@SpringBootApplication
+@RestController
+@EnableRetrofitClients
+public class HelloClientApplication {
+
+	@Autowired
+	HelloClient client;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloClientApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public OkHttpClient.Builder builder() {
+		return new OkHttpClient.Builder();
+	}
+
+	@GetMapping("/")
+	public String hello() throws IOException {
+		return client.hello().execute().body();
+	}
+
+	@RetrofitClient("HelloServer")
+	interface HelloClient {
+		@GET("/")
+		Call<String> hello();
+	}
+}
